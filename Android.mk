df8cad5c49901a3341c53d5dc32abd176f4b9af8
ctxgaming <patrick_pedersen_02032001@yahoo.com>
2015-10-31 21:55:41 +0100

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter gts28wifi, $(TARGET_DEVICE)),)
+
+$(shell mkdir -p out/target/product/gts28wifi/obj/KERNEL_OBJ/drivers/net/wireless/bcmdhd4358)
+$(shell mkdir -p out/target/product/gts28wifi/obj/KERNEL_OBJ/drivers/sensorhub/brcm/factory)
+$(shell mkdir -p out/target/product/gts28wifi/obj/KERNEL_OBJ/drivers/media/platform/exynos/fimc-is/sensor)
+$(shell mkdir -p out/target/product/gts28wifi/obj/KERNEL_OBJ/security/tima_uevent)
+
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+endif
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo cm_gts28wifi-eng
+add_lunch_combo cm_gts28wifi-userdebug
