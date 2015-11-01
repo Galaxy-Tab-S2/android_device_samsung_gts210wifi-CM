4b4c531e5465625f3a3e7d27e5ba4e2568c75f09
ctxgaming <patrick_pedersen_02032001@yahoo.com>
2015-11-01 13:54:19 +0100

    Added some fstab stuff and removed ril related audio lib

--- audio/Android.mk
+++ audio/Android.mk
@@ -21,16 +21,12 @@ LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := audio_hw.c ril_interface.c
+LOCAL_SRC_FILES := audio_hw.c
 
 LOCAL_C_INCLUDES += \
 	external/tinyalsa/include \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils) \
 	$(call include-path-for, audio-route) \
-	hardware/samsung/ril/libsecril-client
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl \
-	libaudioroute libsecril-client
 
 include $(BUILD_SHARED_LIBRARY)
--- device.mk
+++ device.mk
@@ -93,31 +93,6 @@ PRODUCT_PACKAGES += \
     libcec
 
 ###########################################################
-### RADIO
-###########################################################
-
-# cpboot-daemon for modem
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
-
-PRODUCT_PACKAGES += \
-    libsecril-client \
-    libsecril-client-sap
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libargs=-d /dev/ttyS0 \
-    rild.libpath=/system/lib/libsec-ril.so \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    ro.telephony.ril_class=SlteRIL \
-    ro.use_data_netmgrd=false \
-    persist.data.netmgrd.qos.enable=false \
-    ro.data.large_tcp_window_size=true \
-    telephony.lteOnCdmaDevice=0 \
-    persist.radio.add_power_save=1 \
-    persist.radio.apm_sim_not_pwdn=1
-
-###########################################################
 ### WIFI
 ###########################################################
 
