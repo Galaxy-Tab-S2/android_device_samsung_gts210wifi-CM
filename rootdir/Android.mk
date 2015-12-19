3b83127d3a556acd9b345b84c0ed44f709bffe38
Jan Huijsmans <huysmans@koffie.nu>
2015-12-19 18:34:47 +0100

    add ueventd.samsungexynos5433.rc
    Change-Id: I91d8ca63010879c83afa458e8e2d76f2f9010165

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= ueventd.samsungexynos5433.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES		:= etc/ueventd.samsungexynos5433.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.universal5433
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
