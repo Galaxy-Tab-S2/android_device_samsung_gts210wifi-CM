0649ddf4efaf6f986059dfab9c6c777b831c0183
CTXz <ctx.xda@gmail.com>
2016-04-19 13:44:29 +0200

    kernel: Build unified kernel
    * Kernel path now gets defined in the common tree
    
    Change-Id: I6997095a2ca4fe83891dea4dce6a51e9f375982b
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,9 +4,7 @@ include device/samsung/gts2-common/BoardConfigCommon.mk
 LOCAL_PATH := device/samsung/gts210wifi
 
 # Kernel
-# TODO : Unify kernel
 TARGET_KERNEL_CONFIG := cm_exynos5433-gts210wifi_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/gts210wifi
 
 # Extracted with libbootimg
 BOARD_MKBOOTIMG_ARGS += --dt device/samsung/gts210wifi/dtb.img
