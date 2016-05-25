eb45521b66764373feac46d0b7a4042c1bc8c67c
Jan Huijsmans <huysmans@koffie.nu>
2016-05-25 17:07:25 +0200

    Copied from gts210ltexx and adapted for gts210wifi
    Change-Id: I8e6916c2ad3f4c60aacfd3627facb62c00718936

--- device.mk
+++ device.mk
@@ -14,5 +14,27 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.modem.rc
 
-PRODUCT_NAME := full_gts210wifi
-PRODUCT_DEVICE := gts210wifi
+###########################################################
+### RADIO
+###########################################################
+
+# cpboot-daemon for modem
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
+
+###########################################################
+### AUDIO
+###########################################################
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+###########################################################
+### BLUETOOTH
+###########################################################
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# call the proprietary setup
+$(call inherit-product-if-exists, vendor/samsung/gts210wifi/gts210wifi-vendor.mk)
