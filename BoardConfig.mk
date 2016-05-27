6f680ad9baed3dcf6503cf14c0fd40718298b69e
Jan Huijsmans <huysmans@koffie.nu>
2016-05-27 11:23:05 +0200

    Update dtb.img and mkbootimg_args
    Change-Id: Ife6848375bd367016bc94fd8877973f80949d8e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,11 @@
 # Extracted with libbootimg
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/gts210wifi/dtb.img
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/gts210wifi/dtb.img --board SYSMAGIC000KU
 
 # Kernel
-TARGET_KERNEL_CONFIG := exynos5433-gts210wifi_defconfig
+TARGET_KERNEL_CONFIG := cm_exynos5433-gts210lte_defconfig
 
 # RIL
-BOARD_MODEM_TYPE := ss333
+#BOARD_MODEM_TYPE := ss333
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts210wifi,gts210wifixx
