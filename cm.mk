fcf0949de1e9aa0ac54c0d2cb9977b590a34949c
Jan Huijsmans <huysmans@koffie.nu>
2016-06-14 13:27:42 +0200

    T810 doesn't have Telephony
    Change-Id: I6f639a2b91c7152502b64d6a1abf57ae5b4a177d

--- cm.mk
+++ cm.mk
@@ -3,11 +3,11 @@ $(call inherit-product, device/samsung/gts210wifi/device.mk)
 $(call inherit-product, device/samsung/gts2-common/device-common.mk)
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-$(call inherit-product, vendor/cm/config/telephony.mk)
+#$(call inherit-product, vendor/cm/config/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_gts210wifi
